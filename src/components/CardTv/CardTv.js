b1228dfb7adb6ad1cabd4b152ef46a7ea30514d0
KostiantynPolishko <polxs_wp31@student.itstep.org>
2024-06-15 17:22:34 +0300

    3 Done css style of upper part card-tv

--- src/components/CardTv/CardTv.js
+++ src/components/CardTv/CardTv.js
@@ -3,9 +3,16 @@ import cardPhoto from './img/samsung-tv-43-inch.png';
 
 function CardTv(){
   return(
-    <div>
-      <img src={cardPhoto} alt='tv-photo'></img>
-      <p>Component CardTv</p>
+    <div className='card-tv'>
+      <div className='card-tv__img'>
+        <div className='card-tv__img-icon'>
+          <i className="material-symbols-outlined">local_shipping</i>
+        </div>
+        <img src={cardPhoto} alt='tv-photo'></img>
+      </div>
+      <div className='card-tv__date'>
+
+      </div>
     </div>
   );
 }
